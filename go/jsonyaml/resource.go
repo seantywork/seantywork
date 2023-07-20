21fa3d65d71b05d077c819d7bca35359b093f24c
seantywork <seantywork@gmail.com>
2023-07-20 16:43:53 +0900

    fixed malformed tag

--- go/jsonyaml/resource.go
+++ go/jsonyaml/resource.go
@@ -1,45 +1,45 @@
 package main
 
 type Deployment struct {
-	APIVersion string `json,yaml:"apiVersion"`
-	Kind       string `json,yaml:"kind"`
+	APIVersion string `json:"apiVersion" yaml:"apiVersion"`
+	Kind       string `json:"kind" yaml:"kind"`
 	Metadata   struct {
-		Name string `json,yaml:"name"`
-	} `json,yaml:"metadata"`
+		Name string `json:"name" yaml:"name"`
+	} `json:"metadata" yaml:"metadata"`
 	Spec struct {
 		Selector struct {
 			MatchLabels struct {
-				App string `json,yaml:"app"`
-			} `json,yaml:"matchLabels"`
-		} `json,yaml:"selector"`
-		Replicas int `json,yaml:"replicas"`
+				App string `json:"app" yaml:"app"`
+			} `json:"matchLabels" yaml:"matchLabels"`
+		} `json:"selector" yaml:"selector"`
+		Replicas int `json:"replicas" yaml:"replicas"`
 		Template struct {
 			Metadata struct {
 				Labels struct {
-					App string `json,yaml:"app"`
-				} `json,yaml:"labels"`
-			} `json,yaml:"metadata"`
+					App string `json:"app" yaml:"app"`
+				} `json:"labels" yaml:"labels"`
+			} `json:"metadata" yaml:"metadata"`
 			Spec struct {
-				ImagePullSecrets []Deployment_ImagePullSecrets `json,yaml:"imagePullSecrets"`
-				Containers       []Deployment_Containers       `json,yaml:"containers"`
-			} `json,yaml:"spec"`
-		} `json,yaml:"template"`
-	} `json,yaml:"spec"`
+				ImagePullSecrets []Deployment_ImagePullSecrets `json:"imagePullSecrets" yaml:"imagePullSecrets"`
+				Containers       []Deployment_Containers       `json:"containers" yaml:"containers"`
+			} `json:"spec" yaml:"spec"`
+		} `json:"template" yaml:"template"`
+	} `json:"spec" yaml:"spec"`
 }
 
 type Deployment_ImagePullSecrets struct {
-	Name string `json,yaml:"name"`
+	Name string `json:"name" yaml:"name"`
 }
 
 type Deployment_Containers struct {
-	Name            string                        `json,yaml:"name"`
-	Image           string                        `json,yaml:"image"`
-	ImagePullPolicy string                        `json,yaml:"imagePullPolicy"`
-	Ports           []Deployment_Containers_Ports `json,yaml:"ports"`
+	Name            string                        `json:"name" yaml:"name"`
+	Image           string                        `json:"image" yaml:"image"`
+	ImagePullPolicy string                        `json:"imagePullPolicy" yaml:"imagePullPolicy"`
+	Ports           []Deployment_Containers_Ports `json:"ports" yaml:"ports"`
 }
 
 type Deployment_Containers_Ports struct {
-	ContainerPort int `json,yaml:"containerPort"`
+	ContainerPort int `json:"containerPort" yaml:"containerPort"`
 }
 
 var Deployment_json_string = `
