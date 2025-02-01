1c77eab9ceb8a810ab5cc5f48b8c6053d54640ad
seantywork <seantywork@gmail.com>
2025-02-01 08:37:35 +0000

    add gpio signal kernel module

--- kgpio-sock/k/test/kgpiosock.c
+++ kgpio-sock/k/test/kgpiosock.c
@@ -3,11 +3,22 @@
 #include <linux/kernel.h>
 #include <linux/gpio.h>
 #include <linux/interrupt.h> /* IRQ */
+#include <linux/sched.h>
+#include <linux/time.h>
+#include <linux/delay.h>
+#include <linux/workqueue.h>
 
 #define PIN_IN 539 // 27
 #define PIN_OUT 530 // 18
 
-MODULE_LICENSE("GPL");
+
+static DECLARE_WAIT_QUEUE_HEAD(this_wq);
+static int condition = 0;
+
+static struct work_struct job;
+
+
+
 
 static unsigned int irq_number;
 
@@ -16,8 +27,38 @@ static irqreturn_t gpio_irq_handler(int irq, void *dev_id) {
 	return IRQ_HANDLED;
 }
 
-static int __init drv_init(void) {
+static void job_handler(struct work_struct* work){
 
+
+    printk(KERN_INFO "waitqueue handler: %s\n", __FUNCTION__);
+
+    msleep(5000);
+
+    printk(KERN_INFO "wake this process up after 5000ms\n");
+
+	gpio_set_value(PIN_OUT, 1);
+
+
+	int gpioout = gpio_get_value(PIN_OUT);
+
+	printk(KERN_INFO "gpio current state: %d\n", gpioout);
+
+	msleep(1000);
+
+	gpio_set_value(PIN_OUT, 0);
+
+	gpioout = gpio_get_value(PIN_OUT);
+
+	printk(KERN_INFO "gpio current state: %d\n", gpioout);
+
+    condition = 1;
+
+    wake_up_interruptible(&this_wq);
+
+
+}
+
+static int __init drv_init(void) {
 
 
 	if(gpio_request(PIN_IN, "gpio-27-in")) {
@@ -53,18 +94,41 @@ static int __init drv_init(void) {
         gpio_free(PIN_OUT);
 		return -1;
 	}
+
+/*
+	if(request_irq(irq_number, gpio_irq_handler, IRQF_TRIGGER_RISING, "gpio_sock0", NULL) != 0) {
+		printk("gpio_sock: can't request interrupt %d.\n", irq_number);
+		gpio_free(PIN_IN);
+        gpio_free(PIN_OUT);
+		return -1;
+	}
+*/
 	printk("gpio_sock: GPIO 27 is mapped to IRQ %d.\n", irq_number);
 
 	printk("gpio_sock: module is initialized into the kernel.\n");
+
+    INIT_WORK(&job, job_handler);
+
+    schedule_work(&job);
+
+    printk(KERN_INFO "putting to sleep: %s\n", __FUNCTION__);
+
+    wait_event_interruptible(this_wq, condition != 0);
+
+    printk(KERN_INFO "woken up\n");
+
 	return 0;
 }
 
 static void __exit drv_exit(void) {
-	printk("gpio_sock: module is removed from the kernel.\n");
+
 	gpio_free(PIN_IN);
     gpio_free(PIN_OUT);
     free_irq(irq_number, NULL);
+	printk("gpio_sock: module is removed from the kernel.\n");
 }
 
 module_init(drv_init);
 module_exit(drv_exit);
+
+MODULE_LICENSE("GPL");
