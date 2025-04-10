5cafb08e6d1c00210012f936df126dfb7edf4c8e
seantywork <seantywork@gmail.com>
2025-04-10 12:48:00 +0000

    add openssl 3.5.0 setup script

---
+++ tls-quantum-safe/setup_openssl3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+OSSLV="3.5.0"
+
+
+curl -L "https://github.com/openssl/openssl/releases/download/openssl-$OSSLV/openssl-$OSSLV.tar.gz" -o "openssl-$OSSLV.tar.gz"
+
+tar -zxf "openssl-$OSSLV.tar.gz"
+
+cd "openssl-$OSSLV"
+
+./config
+
+make
+
+make test
+
+sudo make install
+
+sudo ldconfig /usr/local/lib64/
+
+#mv /usr/bin/openssl /root/
+
+#ln -s /usr/local/ssl/bin/openssl /usr/bin/openssl
