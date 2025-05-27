d20bba2bbde2153f0c7a6085afa63d7ec866bcf9
seantywork <seantywork@gmail.com>
2025-05-27 13:55:43 +0100

    gpio

---
+++ ksock-gpio/ksock_gpio.c
@@ -0,0 +1,23 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
+#include <linux/string.h>
+
+static int __init ksock_gpio_init(void) {
+
+	return 0;
+}
+
+static void __exit ksock_gpio_exit(void) {
+
+}
+
+module_init(ksock_gpio_init);
+module_exit(ksock_gpio_exit);
+
+MODULE_LICENSE("GPL");
