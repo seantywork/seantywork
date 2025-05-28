3aa215a32c62bd61f9b16712edfdf6950d128ebe
seantywork <seantywork@gmail.com>
2025-05-28 14:12:26 +0100

    ranamed to gpio irqsk

--- kgpio-irqsock/ins.sh
+++ kgpio-irqsock/ins.sh
@@ -27,5 +27,5 @@ CTLIN_LINE=$(echo $CTLIN_LINE | cut -d " " -f 1)
 echo "CTL OUT: $CTLOUT = $CTLOUT_LINE"
 echo "CTL IN : $CTLIN = $CTLIN_LINE"
 
-insmod ksock_gpio.ko gpio_ctl_o=$CTLOUT_LINE gpio_ctl_i=$CTLIN_LINE
+insmod kgpio_irqsk.ko gpio_ctl_o=$CTLOUT_LINE gpio_ctl_i=$CTLIN_LINE
 
