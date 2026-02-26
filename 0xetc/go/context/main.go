ebcb485b1d4307c98eaa0804b6813449b88adc6d
seantywork <seantywork@gmail.com>
2026-02-26 00:47:35 +0000

    go context

--- 0xetc/go/context/main.go
+++ 0xetc/go/context/main.go
@@ -3,16 +3,43 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
+
+var TIMEOUTSEC int = 10
 
 func main() {
 
 	ctx := context.Background()
 
 	if d, ok := ctx.Deadline(); ok {
-		fmt.Printf("yes deadlin: %v\n", d)
+		fmt.Printf("yes deadline: %v\n", d)
 	} else {
 		fmt.Printf("no deadline\n")
 	}
 
+	ctxTimeOut, cancel := context.WithTimeout(ctx, time.Millisecond*1000*time.Duration(TIMEOUTSEC))
+
+	if d, ok := ctxTimeOut.Deadline(); ok {
+		fmt.Printf("yes deadline: %v\n", d)
+	} else {
+		fmt.Printf("no deadline\n")
+	}
+
+	go func() {
+		time.Sleep(time.Millisecond * 5000)
+		fmt.Println("<< calling cancel")
+		cancel()
+	}()
+
+	for {
+		select {
+		case <-ctxTimeOut.Done():
+			fmt.Println("timeout")
+			return
+		default:
+			fmt.Println("waiting...")
+			time.Sleep(time.Second * 1)
+		}
+	}
 }
