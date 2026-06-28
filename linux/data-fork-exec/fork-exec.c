67eede30a6904f12eca620cf0c4c7a5d19d4b1b9
seantywork <seantywork@gmail.com>
2026-06-28 13:24:14 +0900

    test

--- linux/data-fork-exec/fork-exec.c
+++ linux/data-fork-exec/fork-exec.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <pthread.h>
+int val = 0;
+
+
+void* rrr(void* varg){
+	val = 5;
+	pthread_exit(NULL);
+}
 
 int main( void ) {
 	char *argv[1] = {NULL};
@@ -14,15 +22,17 @@ int main( void ) {
 		// for new session
 		// signal effective
 		// setsid();
-
-        printf( "child process pid: %d\n", (int)getpid() );
+		pthread_t tid;
+		pthread_create(&tid, NULL, rrr, NULL);
+		pthread_join(tid, NULL);
+        printf( "child process pid: %d: val: %d\n", (int)getpid(), val);
 		execvp( "ls", argv );
 	}
 
 	sleep(2);
 
 	printf( "child pid: %d\n", pid);
-	printf( "parent process pid: %d\n", (int)getpid() );
+	printf( "parent process pid: %d: val: %d\n", (int)getpid(), val);
 
 	return 0;
 }
