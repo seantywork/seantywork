6604ccf1429f889c1983e9e64c585049c63979b9
seantywork <seantywork@gmail.com>
2026-07-02 03:51:08 +0000

    more to pid

--- linux/data-fork-exec/fork-exec.c
+++ linux/data-fork-exec/fork-exec.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <pthread.h>
 int val = 0;
@@ -10,12 +11,9 @@ void* rrr(void* varg){
 	pthread_exit(NULL);
 }
 
-int main( void ) {
-	char *argv[1] = {NULL};
-
+int main(int argc, char** argv) {
+	char *argv2[1] = {NULL};
     printf( "parent process pid: %d\n", (int)getpid() );
-
-
 	int pid = fork();
 
 	if ( pid == 0 ) {
@@ -23,13 +21,29 @@ int main( void ) {
 		// signal effective
 		// setsid();
 		pthread_t tid;
-		pthread_create(&tid, NULL, rrr, NULL);
-		pthread_join(tid, NULL);
-        printf( "child process pid: %d: val: %d\n", (int)getpid(), val);
-		execvp( "ls", argv );
+		int* fault = NULL;
+		if(argc > 1){
+			printf("here comes the fault\n");
+			fault[10] = 1;
+		} else {
+			pthread_create(&tid, NULL, rrr, NULL);
+			pthread_join(tid, NULL);
+			printf("child process pid: %d: val: %d\n", (int)getpid(), val);
+			execvp("ls", argv2);
+		}
+	} else {
+		int status = 0;
+		wait(&status);
+		if(WIFEXITED(status)){
+			printf("exited, status=%d\n", WEXITSTATUS(status));
+		} else if(WIFSIGNALED(status)){
+			printf("killed by signal %d\n", WTERMSIG(status));
+		} else if(WIFSTOPPED(status)){
+			printf("stopped by signal %d\n", WSTOPSIG(status));
+		} else if(WIFCONTINUED(status)){
+			printf("continued\n");
+		}
 	}
-
-	sleep(2);
 
 	printf( "child pid: %d\n", pid);
 	printf( "parent process pid: %d: val: %d\n", (int)getpid(), val);
